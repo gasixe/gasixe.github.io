62c742971c72e439b20b94c493ce88b996937df0
gasixe <gasixe@users.noreply.github.com>
2025-12-13 09:40:33 +0000

    deploy: gasixe/gasixe@5ba38ffea71c3a7431662fd2f0c1d1faf747e78b

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/13/ceshi/","2025/11/30/应用/"];function toRandomPost(){
+var posts=["2025/11/30/应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
