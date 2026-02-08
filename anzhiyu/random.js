0645d91c7e62f1c935f149f3f7b0e7a08bdd21ac
gasixe <gasixe@users.noreply.github.com>
2026-02-08 14:45:56 +0000

    deploy: gasixe/gasixe@1ff9756d8edc3dc3e18aa51d8cac0a4a329f818f

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/30/应用/"];function toRandomPost(){
+var posts=["2026/02/08/免费VPS/","2025/11/30/应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
